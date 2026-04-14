d83124c323f3fb17cfba16960a092e104c3d56f9
Juan Pablo Landa <juanpi9landa@gmail.com>
2026-04-13 21:23:22 -0300

    Add TWA manifest file for El Lote Puntos application

---
+++ app/src/main/java/app/netlify/rfwdsfgdnfdfgegdbf0pbodgt0odnbot348n/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.rfwdsfgdnfdfgegdbf0pbodgt0odnbot348n.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
